839dc880db798ff0098f14195c459bbd373696f2
Krsicka Jakub <jakub.krsicka@tieto.com>
2019-07-09 09:45:05 +0200

    Third commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        User jakob = new User ("Jakub",21);
+        System.out.println(jakob);
     }
 }
--- src/com/company/User.java
+++ src/com/company/User.java
@@ -1,3 +1,5 @@
+package com.company;
+
 public class User {
     private String name;
     private int age;
